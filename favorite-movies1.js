bc7f3398d713b42a1440c61c951bb144419a0aef
Fatema Kabir <fatema.kabir@pursuit.org>
2021-12-02 17:22:06 -0500

    additionalchange

---
+++ favorite-movies1.js
@@ -0,0 +1,18 @@
+function createPerson(name, age) {
+    return {
+      name,
+      age,
+      friends: [],
+    };
+  }
+  
+  function addPersonAsFriend(personB, personA) {
+    personB.friends.push(personA);
+    return personB;
+  }
+  
+  module.exports = {
+    createPerson,
+    addPersonAsFriend,
+  };
+  
